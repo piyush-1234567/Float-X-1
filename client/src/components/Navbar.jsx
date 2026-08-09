994e2d68268f43186177c6aea74c8e690365cce3
Piyush Jain <piyushjain07032004@gmail.com>
2026-08-09 23:38:15 +0530

    Started making the navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+export default function Navbar(){
+    return (
+        <nav>
+            <div>
+                Float-X
+            </div>
+            <div>
+                <a href="">Home</a>
+                <a href="">Services</a>
+                <a href="">Portfolio</a>
+                <a href="">Process</a>
+                <a href="">Pricing</a>
+                <a href="">About Us</a>
+                <a href="">Contact</a>
+            </div>
+            <button>Book a Free Call</button>
+        </nav>
+    );
+}
