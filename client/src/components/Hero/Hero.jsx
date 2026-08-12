46cb97b5269d458696cf3b19fe5650045e098687
Piyush Jain <piyushjain07032004@gmail.com>
2026-08-12 23:53:56 +0530

    working on hero section-1

---
+++ client/src/components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import "./Hero.css";
+export default function Hero(){
+    return (
+        <section className="hero">
+            <span>🚀 Digital Agency for Modern Brands</span>
+
+            <h1>
+                We Build Digital Experiences
+                <br />
+                That <strong>Drive Real Growth</strong>
+            </h1>
+            <p>
+                Creative websites and digital solutions for modern businesses.
+                <br />
+                designed to help your business scale smarter and faster.
+            </p>
+        </section>
+    )
+}
